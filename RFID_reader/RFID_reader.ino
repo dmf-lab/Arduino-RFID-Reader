8992688feb85ba4a65fb76e8848360fbc9017a8e
David Mathias <d.r.f.mathias@gmail.com>
2018-06-15 11:44:01 +0100

    Arduino code commit

---
+++ RFID_reader/RFID_reader.ino
@@ -0,0 +1,82 @@
+const int tagLen = 16;
+const int idLen = 13;
+
+// Empty array to hold a freshly scanned tag
+char newTag[idLen];
+
+void setup() {
+  // Starts the serial ports
+   Serial.begin(9600);
+   Serial1.begin(9600);
+}
+
+void loop() {
+  // Counter for the newTag array
+  int i = 0;
+  // Variable to hold each byte read from the serial buffer
+  int readByte;
+  // Flag so we know when a tag is over
+  boolean tag = false;
+
+  // This makes sure the whole tag is in the serial buffer before
+  // reading, the Arduino can read faster than the ID module can deliver!
+  if (Serial1.available() == tagLen) {
+    tag = true;
+  }
+
+  if (tag == true) {
+    while (Serial1.available()) {
+      // Take each byte out of the serial buffer, one at a time
+      readByte = Serial1.read();
+
+      /* This will skip the first byte (2, STX, start of text) and the last three,
+      ASCII 13, CR/carriage return, ASCII 10, LF/linefeed, and ASCII 3, ETX/end of 
+      text, leaving only the unique part of the tag string. It puts the byte into
+      the first space in the array, then steps ahead one spot */
+      
+      if (readByte != 2 && readByte!= 13 && readByte != 10 && readByte != 3) {
+        newTag[i] = readByte;
+        i++;
+      }
+      
+      // If we see ASCII 3, ETX, the tag is over
+      if (readByte == 3) {
+        tag = false;
+      }
+
+    }
+  }
+
+
+  // don't do anything if the newTag array is full of zeroes
+  if (strlen(newTag)== 0) {
+    return;
+  }
+
+  else {
+
+        // This prints out unknown cards so you can add them to your knownTags as needed
+        Serial.print("Tag ID: ");
+        Serial.print(newTag);
+        Serial.println();
+  }
+
+  // Once newTag has been checked, fill it with zeroes
+  // to get ready for the next tag read
+  for (int c=0; c < idLen; c++) {
+    newTag[c] = 0;
+  }
+}
+
+// This function steps through both newTag and one of the known
+// tags. If there is a mismatch anywhere in the tag, it will return 0,
+// but if every character in the tag is the same, it returns 1
+int checkTag(char nTag[], char oTag[]) {
+    for (int i = 0; i < idLen; i++) {
+      if (nTag[i] != oTag[i]) {
+        return 0;
+      }
+    }
+  return 1;
+}
+
